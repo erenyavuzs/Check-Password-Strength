6f1797b0a78a0f67625bbcac55483294c3aa4bb5
Eren Yavuz <109018781+erenyavuzs@users.noreply.github.com>
2024-08-19 15:44:39 +0300

    refactor & check

--- main.py
+++ main.py
@@ -14,15 +14,17 @@ def test_multiple_passwords(password_file):
     try:
         with open(password_file, 'r') as passwords:
             for password in passwords:
-                result = zxcvbn(password.strip('/n'))
-                print('\n[+] #########################') #for readability
-                print(f"Value: {result['password']}")
-                print(f"Password Score: {result['score']}/4")
-                print(f"Crack Time: {result['crack_times_display']['offline_slow_hashing_1e4_per_second']}")
-                print(f"Feedback: {result['feedback']['suggestions']}")
-
-    except Exception:
-        print('[!] Please make sure to specify an accessible file containing passwords.')
+                password = password.strip()  
+                if password:  
+                    result = zxcvbn(password)
+                    print("\n") # for readability
+                    print(f"Value: {result['password']}")
+                    print(f"Password Score: {result['score']}/4")
+                    print(f"Crack Time: {result['crack_times_display']['offline_slow_hashing_1e4_per_second']}")
+                    print(f"Feedback: {result['feedback']['suggestions']}")
+                    print("\n") # for readability
+    except Exception as e:
+        print(f'[!] Error: {str(e)}')
 
 
 if len(sys.argv) == 2:
@@ -30,4 +32,5 @@ if len(sys.argv) == 2:
 elif len(sys.argv) == 1:
     test_single_password()
 else:
-    print()
+    print("\n[!] Usage: python3 main.py <file> (for a file containing passwords) or \
+        \npython3 main.py (for a single password)")
